eea9083a8d2cebf7ef48e05ccede44f056e02cde
Michael Drozdowski <mjd76@njit.edu>
2020-11-23 19:14:36 -0500

    Feature: Implement add template

--- app/app.py
+++ app/app.py
@@ -61,10 +61,10 @@ def form_insert_get():
 @app.route('/player/new', methods=['POST'])
 def form_insert_post():
     cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
-                 request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
-                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'))
-    sql_insert_query = """INSERT INTO mlbPlayers (plName,plTeam, plPosition, plHieght, plWeight, plAge) VALUES (%s,%s, %s,%s, %s,%s)"""
+    inputData = (request.form.get('plName'), request.form.get('plTeam'), request.form.get('plPosition'),
+                 request.form.get('plHeight'), request.form.get('plWeight'),
+                 request.form.get('plAge'))
+    sql_insert_query = """INSERT INTO mlbPlayers (plName,plTeam, plPosition, plHeight, plWeight, plAge) VALUES (%s,%s, %s,%s, %s,%s)"""
     cursor.execute(sql_insert_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
